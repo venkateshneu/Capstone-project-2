80d96def7e19584a821a1570dc080ad1278070ce
venkateshneu <guri.venkatesh1996@gmail.com>
2024-08-20 03:31:10 -0400

    adding requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="venky",
+    author_email="guri.venkatesh1996@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
