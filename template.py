4d6a01b321d0dd1ad0fe0cc8931da3a141df4c63
venkateshneu <guri.venkatesh1996@gmail.com>
2024-08-21 12:47:50 -0400

    data_ingestion

--- template.py
+++ template.py
@@ -17,6 +17,7 @@ list_of_files = [
     f"{project_name}/configuration/gcloud_syncer.py",
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
     f"{project_name}/constants/__init__.py",
     f"{project_name}/exception/__init__.py",
     f"{project_name}/logger/__init__.py",
